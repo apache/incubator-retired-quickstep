c456c0b48f93e13cf290e3084464097f6c105887
Jianqiao Zhu <jianqiao@cs.wisc.edu>
2016-04-08 10:12:19 -0500

    Worker delete WorkOrder before sendWorkOrderCompleteMessage (#145)

--- query_execution/Worker.cpp
+++ query_execution/Worker.cpp
@@ -58,11 +58,11 @@ void Worker::run() {
         WorkerMessage message(*static_cast<const WorkerMessage*>(tagged_message.message()));
         DCHECK(message.getWorkOrder() != nullptr);
         message.getWorkOrder()->execute();
+        delete message.getWorkOrder();
 
         sendWorkOrderCompleteMessage(annotated_msg.sender,
                                      message.getRelationalOpIndex(),
                                      tagged_message.message_type() == kRebuildWorkOrderMessage);
-        delete message.getWorkOrder();
         break;
       }
       case kPoisonMessage: {
