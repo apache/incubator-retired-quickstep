jianqiao/incubator-quickstep

Add BitVectorExactFilter as a LIP filter and supports Join-to-Semijoin transformation.

query_optimizer/ExecutionGenerator.hpp
@@ -46,6 +46,7 @@
 #include "query_optimizer/physical/CreateTable.hpp"
 #include "query_optimizer/physical/DeleteTuples.hpp"
 #include "query_optimizer/physical/DropTable.hpp"
+#include "query_optimizer/physical/FilterInjection.hpp"
 #include "query_optimizer/physical/HashJoin.hpp"
 #include "query_optimizer/physical/InsertSelection.hpp"
 #include "query_optimizer/physical/InsertTuple.hpp"
@@ -246,6 +247,13 @@ class ExecutionGenerator {
    * @param physical_plan The SharedSubplanReference to be converted.
    */
   void convertSharedSubplanReference(const physical::SharedSubplanReferencePtr &physical_plan);
+
+  /**
+   * @brief Converts a FilterInjection to a BuildLIPFilter operator.
+   *
+   * @param physical_plan The FilterInjection to be converted.
+   */
+  void convertFilterInjection(const physical::FilterInjectionPtr &physical_plan);
 
   /**
    * @brief Converts a HashJoin to BuildHash, HashJoin and

apache/incubator-retired-quickstep

query_optimizer/ExecutionGenerator.hpp
@@ -46,6 +46,7 @@
 #include "query_optimizer/physical/CreateTable.hpp"
 #include "query_optimizer/physical/DeleteTuples.hpp"
 #include "query_optimizer/physical/DropTable.hpp"
+#include "query_optimizer/physical/FilterJoin.hpp"
 #include "query_optimizer/physical/HashJoin.hpp"
 #include "query_optimizer/physical/InsertSelection.hpp"
 #include "query_optimizer/physical/InsertTuple.hpp"
@@ -246,6 +247,13 @@ class ExecutionGenerator {
    * @param physical_plan The SharedSubplanReference to be converted.
    */
   void convertSharedSubplanReference(const physical::SharedSubplanReferencePtr &physical_plan);
+
+  /**
+   * @brief Converts a FilterJoin to a BuildLIPFilter operator.
+   *
+   * @param physical_plan The FilterJoin to be converted.
+   */
+  void convertFilterJoin(const physical::FilterJoinPtr &physical_plan);
 
   /**
    * @brief Converts a HashJoin to BuildHash, HashJoin and